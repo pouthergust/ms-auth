963f19860a8e44a11ff032e23fd7f14fbc78def5
Gabriel H <gabriel.ghvn@gmail.com>
2022-01-07 22:25:43 -0300

    Basic Auth added

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -3,37 +3,19 @@ import ForbiddenError from "../models/errors/forbidden.error.model";
 import userRepository from "../repositories/user.repositories";
 import JWT from 'jsonwebtoken';
 import { StatusCodes } from "http-status-codes";
+import basicAuthenticationMiddleware from "../middleware/basic-authentication.middleware";
 
 const authorizationRoute = Router();
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const authorizationHeader = req.headers['authorization'] 
-
-        if (!authorizationHeader) {
-            throw new ForbiddenError('Credenciais não informadas')
-        }
-
-        const [authenticationType, token] = authorizationHeader.split(' ')
-
-        if (authenticationType !== 'Basic' || !token) {
-            throw new ForbiddenError('Tipo de autenticação inválido')
-        }
-
-        const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
-        const [username, password] = tokenContent.split(':')
-
-        if (!username || !password) {
-            throw new ForbiddenError('Credenciais não informadas')
-        }
-    
-        const user = await userRepository.findByUsernameAndPassword(username, password)
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
+    try {    
+        const user = req.user
 
         if (!user) {
-            throw new ForbiddenError('Usuario ou senha invalidos')
+            throw new ForbiddenError('Usuario não informado')
         }
 
-        const jwt = JWT.sign({ username: user.username }, 'my_secret_key', { subject: user?.uuid })
+        const jwt = JWT.sign({ username: user.username }, 'my_secret_key', { subject: user.uuid })
         res.status(StatusCodes.OK).json({ token: jwt })
     } catch (error) {
         next(error)
